6090cb06e28e0ee762af07739fd82eedbef542f8
Siôn Le Roux <sinisterstuf@gmail.com>
2013-10-11 00:33:03 +0200

    Initial commit: a working clock counting only seconds
    My work so far. Each LED is connected to a pin on an Arduino. First we
    get the seconds part of the current time, then convert it to binary and
    use that to send the HIGH or LOW signals to the pins to show the time
    (just the seconds) on the LEDs. This is repeated as fast as possible.
    
    Most of the code was originally written in C on a train and then
    modified to run on the Arduino as soon as I had one to test it on.

---
+++ binary-clock.ino
@@ -0,0 +1,57 @@
+#include <Time.h>
+
+// we've just got 5 LEDs here, which is supposed to show an hour 0-23
+#define NUM_LEDS 6
+
+// represents a led, which has an identifiying pin number and a state on/off
+typedef struct {
+	int num;
+	boolean on;
+} led;
+
+// array of connected leds
+led leds[NUM_LEDS] = {{13, false}, {12, false}, {11, false}, {10, false}, {9, false}, {8, false}};
+
+// FUNCTION DEFS
+char *int2bin(int, char*, int);
+
+// converts an integer number into a string of 0s and 1s
+char *int2bin(int a, char *buffer, int buf_size) {
+	int i = 0; // for the for loop
+	buffer += (buf_size - 1);
+
+	for (i = buf_size - 1; i >= 0; i--) {
+		*buffer-- = (a & 1) + '0';
+		a >>= 1;
+	}
+
+	return buffer;
+}
+
+//TODO: find out why this outputs 6 (00110) instead of 24 (11000)
+void setup() {
+	// initialize the pins as output and set all to LOW
+	for(int i = 0; i < NUM_LEDS; i++) {
+		pinMode(leds[i].num, OUTPUT);
+		digitalWrite(leds[i].num, LOW);
+	}
+}
+
+void loop() {
+	int number = second();
+	
+	char buffer[NUM_LEDS + 1]; //the +1 is for the \0 null terminator
+	buffer[NUM_LEDS] = '\0'; 
+	int2bin(number, buffer, NUM_LEDS);
+	for(int i; i < NUM_LEDS; i++) {
+		if ( buffer[i] == '0' ) {
+			leds[i].on = false;
+			digitalWrite(leds[i].num, LOW);
+		} else if ( buffer[i] == '1' ) {
+			leds[i].on = true;
+			digitalWrite(leds[i].num, HIGH);
+		} else {
+			return;
+		}
+	}
+}
