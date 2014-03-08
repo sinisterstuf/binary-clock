5b8dd51c6d8217d141efb8875d019e9874e2c3ac
Siôn Le Roux <sinisterstuf@gmail.com>
2014-03-08 12:01:35 +0100

    Fix dependency in header file
    It's been a while since I wrote C. Apparently I need to include the
    binclock C source file in the binclock header file so that the functions
    are callable when including the header file from other files. This also
    means I can put my struct in the C file where it belongs and the code
    compiles!

--- lib/binclock/binclock.h
+++ lib/binclock/binclock.h
@@ -1,10 +1,5 @@
 #include <stdbool.h>
-
-// represents a led, which has an identifiying pin number and a state on/off
-typedef struct {
-    int num;
-    bool on;
-} led;
+#include "binclock.c"
 
 // converts an integer number into a string of 0s and 1s
 char* int2bin(int, char*, int);
